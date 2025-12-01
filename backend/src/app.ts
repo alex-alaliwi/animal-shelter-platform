c6a2b864c5e962b5eda059ee5c7425b9fcfab55d
Ahmad Alaliwi <alaliwiahmad@hotmail.com>
2025-11-30 21:19:13 -0800

    Add simple request logger middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { json } from "body-parser";
 import healthRouter from './routes/health'
+import { logger } from './middleware/logger';
 
 const app = express()
 
 app.use(json());
+app.use(logger);
 
 app.use("/health", healthRouter);
 
