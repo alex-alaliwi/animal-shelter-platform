d83b7f8fca667d3de8f90ad2559bf6f6a5b7197e
Ahmad Alaliwi <alaliwiahmad@hotmail.com>
2025-12-01 19:19:26 -0800

    Add animal model and basic animals route skeleton

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { json } from "body-parser";
 import healthRouter from './routes/health'
 import { logger } from './middleware/logger';
+import animalsRouter from './routes/animals';
 
 const app = express()
 
 app.use(json());
+app.use('/api/animals', animalsRouter);
 app.use(logger);
 
 app.use("/health", healthRouter);
---
+++ backend/src/models/animal.model.ts
@@ -0,0 +1,7 @@
+export interface Animal {
+  id: string;
+  name: string;
+  species: string;
+  age: number;
+  status: "available" | "adopted";
+}
